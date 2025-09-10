0946bb50dc790307978d34e9945a058bcf1f3d25
Ozancico <ozancicek.oc@gmail.com>
2025-09-10 13:45:15 +0200

    Read PORT from environment; easier to run on custom ports

--- app.py
+++ app.py
@@ -144,4 +144,5 @@ def health_check():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.getenv('PORT', '5000'))
+    app.run(debug=True, host='0.0.0.0', port=port)
